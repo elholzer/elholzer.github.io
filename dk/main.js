38c43dd247486212b9120d7d6a28c73e766e15d4
dirt man <17elholz@sheboyganfalls.k12.wi.us>
2017-01-27 16:00:32 +0000

    donkey kong

---
+++ dk/main.js
@@ -0,0 +1,5 @@
+function playHesTheLeaderOfTheBunch(){
+var hesTheLeaderOfTheBunch = new Audio("donkey_kong.mp3");
+hesTheLeaderOfTheBunch.play();
+}
+$("#donkey").on("click", playHesTheLeaderOfTheBunch);
