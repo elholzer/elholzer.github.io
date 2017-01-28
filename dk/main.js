f3e36e5af74d4a2bbd35939ca065b81f9c5cecaf
dirt man <17elholz@sheboyganfalls.k12.wi.us>
2017-01-28 00:40:47 +0000

    expand

--- dk/main.js
+++ dk/main.js
@@ -1,5 +1,36 @@
-function playHesTheLeaderOfTheBunch(){
-var hesTheLeaderOfTheBunch = new Audio("donkey_kong.mp3");
-hesTheLeaderOfTheBunch.play();
+
+//function playHesTheLeaderOfTheBunch(){
+//var hesTheLeaderOfTheBunch = new Audio("donkey_kong.mp3");
+//hesTheLeaderOfTheBunch.play();
+//}
+//$("#donkey").on("click", playHesTheLeaderOfTheBunch);
+
+function sayDonkeyKong(){
+var donkeyKong = new Audio("donkey_kong.mp3");
+donkeyKong.play();
 }
-$("#donkey").on("click", playHesTheLeaderOfTheBunch);
+$("#donkey").on("click", sayDonkeyKong);
+
+function sayTinyKong(){
+    var tinyKong = new Audio("tiny_kong.mp3");
+    tinyKong.play();
+}
+$("#tiny").on("click", sayTinyKong);
+
+function sayLankyKong(){
+    var lankyKong = new Audio("lanky_kong.mp3");
+    lankyKong.play();
+}
+$("#lanky").on("click", sayLankyKong);
+
+function sayDiddyKong(){
+    var diddyKong = new Audio("diddy_kong.mp3");
+    diddyKong.play();
+}
+$("#diddy").on("click", sayDiddyKong);
+
+function sayChunkyKong(){
+    var chunkyKong = new Audio("chunky_kong.mp3");
+    chunkyKong.play();
+}
+$("#chunky").on("click", sayChunkyKong);
